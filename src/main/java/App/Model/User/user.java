adc68b7f7b7a84315e635694d51de91710f28e65
Bouguedra-Adem <32870464+Bouguedra-Adem@users.noreply.github.com>
2019-06-22 00:06:03 +0200

    commit2

--- src/main/java/App/Model/User/user.java
+++ src/main/java/App/Model/User/user.java
@@ -1,5 +1,30 @@
 package App.Model.User;
 
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="User")
+
 public class user {
+	@Id 
+	   @GeneratedValue(strategy=GenerationType.AUTO)
+	   private int id;
+	   private String Nom;
+	   private String Prenom;
+	   private String Nss;
+	   private String Organisme;
+	   private String NumTelf;
+	   @OneToMany(mappedBy="User")
+	   private List<DemandeRessource> DemandeRess;
+	   
+	   
+	   
 
 }
